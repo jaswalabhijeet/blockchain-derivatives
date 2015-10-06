70ca516800519397324717780c60764780717583
billmarino2 <williammarino@Williams-MacBook-Pro.local>
2015-10-05 20:51:13 -0400

    Add Procfile, requirements.txt
    Add Procfile, requirements.txt

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -139,5 +139,8 @@ def show_user_profile(username):
 
 if __name__ == '__main__':
     #app.debug = True
-    app.run(debug=True, port=5001)
+    #app.run(debug=True)
+    #app.run(debug=True, port=5001)
     #app.run(host='0.0.0.0')   #turn this on later when you go to another server
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
