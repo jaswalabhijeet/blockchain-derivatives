0a262067ef86433d40c21136ec700e5fae9524f8
Bill Marino <billmarino@gmail.com>
2015-10-13 02:31:08 -0400

    Clean Up Comments in app.py

--- app.py
+++ app.py
@@ -181,6 +181,3 @@ if __name__ == '__main__':
     #app.run(host='0.0.0.0')   #turn this on later when you go to another server
     port = int(os.environ.get('PORT', 5001))
     app.run(host='0.0.0.0', port=port)
-
-
-
