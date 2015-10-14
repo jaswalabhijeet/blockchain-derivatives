14d979803d0bdcb98cb8190fe86a12aeb90b5bca
Bill Marino <billmarino@gmail.com>
2015-10-14 01:28:40 -0400

    Remove All DB References from app.py

--- app.py
+++ app.py
@@ -46,39 +46,40 @@ def main():
 @app.route('/futureethereum')
 def main_future():
     #return render_template('futureethereum.html')  #this was only thing here initially
-    error = None
-    if request.method == 'POST':
-        if request.form['buyerethereumaddress'] == null:
-            error = 'Invalid buyer ethereum address'
-        elif request.form['seller ethereum address'] == null:
-            error = 'Invalid seller ethereum address'
-        elif request.form['deliverydate'] == null:
-            error = 'Invalid delivery date'
-        elif request.form['numberofunits'] == null:
-            error = 'Invalid number of units'
-        elif request.form['commodityname'] == null:
-            error = 'Invalid commodity name'
-        elif request.form['price'] == null:
-            error = 'Invalid price'
-        elif request.form['margin'] == null:
-            error = 'Invalid margin'
-        elif request.form['soliditycodeinitial'] == null:
-            error = 'Invalid solidity code initial'
-        else:
+    #error = None
+    #if request.method == 'POST':
+        #if request.form['buyerethereumaddress'] == null:
+            #error = 'Invalid buyer ethereum address'
+        #elif request.form['seller ethereum address'] == null:
+            #error = 'Invalid seller ethereum address'
+        #elif request.form['deliverydate'] == null:
+            #error = 'Invalid delivery date'
+        #elif request.form['numberofunits'] == null:
+            #error = 'Invalid number of units'
+        #elif request.form['commodityname'] == null:
+            #error = 'Invalid commodity name'
+        #elif request.form['price'] == null:
+            #error = 'Invalid price'
+        #elif request.form['margin'] == null:
+            #error = 'Invalid margin'
+        #elif request.form['soliditycodeinitial'] == null:
+            #error = 'Invalid solidity code initial'
+        #else:
             #session['logged_in'] = True
-            flash('Great')
+            #flash('Great')
             #__tablename__ = "FuturesContractsCreated"
-            urllib.uses_netloc.append("postgres")
-            url = urllib.parse(os.environ["DATABASE_URL"])
-            con = psycopg2.connect(database=url.path[1:],user=url.username,password=url.password,host=url.hostname,port=url.port)
+            #urllib.uses_netloc.append("postgres")
+            #url = urllib.parse(os.environ["DATABASE_URL"])
+            #con = psycopg2.connect(database=url.path[1:],user=url.username,password=url.password,host=url.hostname,port=url.port)
             #con = psycopg2.connect("dbname=test user=postgres") 	
-            cur = con.cursor()
-            cur.execute("INSERT INTO 'FuturesContractsCreated' (blockchainderivativesid,buyerethereumaddress,sellerethereumaddress, deliverydate, numberofunits, commodityname, price, margin, soliditycodeinitial)) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)",[app.config['USERNAME'],request.form['buyerethereumaddress'],request.form['sellerethereumaddress'], request.form['deliverydate'], request.form['numberofunits'], request.form['commodityname'], request.form['price'], request.form['margin'], request.form['soliditycodeinitial']])
-            con.commit()
-            cur.close()
-            con.close()
+            #cur = con.cursor()
+            #cur.execute("INSERT INTO 'FuturesContractsCreated' (blockchainderivativesid,buyerethereumaddress,sellerethereumaddress, deliverydate, numberofunits, commodityname, price, margin, soliditycodeinitial)) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)",[app.config['USERNAME'],request.form['buyerethereumaddress'],request.form['sellerethereumaddress'], request.form['deliverydate'], request.form['numberofunits'], request.form['commodityname'], request.form['price'], request.form['margin'], request.form['soliditycodeinitial']])
+            #con.commit()
+            #cur.close()
+            #con.close()
             #return render_template('futureethereum.html')
-            return redirect(url_for('main_future'))
+            #return redirect(url_for('main_future'))
+    #return render_template('futureethereum', error=error)
     return render_template('futureethereum', error=error)
 
 #check out this page if necessary for better forms: http://stackoverflow.com/questions/20837209/flask-wtform-save-form-to-db
@@ -94,7 +95,7 @@ def main_swap():
 if __name__ == '__main__':
     app.run(debug=True)
     #app.run(host='0.0.0.0')   #turn this on later when you go to another server
-    #port = int(os.environ.get('PORT', 5000))
+    #port = int(os.environ.get('PORT', 5001))
     #app.run(host='0.0.0.0', port=port)
     port = int(os.environ.get("PORT", 33507))
     app.run(host='0.0.0.0', port=port)
