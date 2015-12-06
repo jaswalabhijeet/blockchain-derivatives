9641dca23d47af84125e8a427895bc516ee616d4
Bill Marino <billmarino@gmail.com>
2015-12-06 01:44:30 -0500

    Add Call Options Table and Related Funcationality

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+# imports
+
 import os
 from flask import Flask, render_template, request, session, g, redirect, url_for, abort, flash, Blueprint, \
     send_from_directory, current_app
@@ -17,20 +19,16 @@ from flask_restful import Resource, Api
 from decimal import Decimal
 from werkzeug.datastructures import ImmutableMultiDict
 
-#html = urlopen("http://www.google.com/")
-#print(html.read())
+# configuration
 
 app = Flask(__name__)
 api = Api(app)
 app.secret_key = "super secret key"
-
-# in the command line, maybe do: 'run python' and then 'from app import db' and then 'db.create_all()' and to leave: 'exit()'
-
 db = SQLAlchemy(app)
-
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.ERROR)
 
+# database instantiation
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -51,12 +49,10 @@ class User(db.Model, UserMixin):
         return False
 
     def get_id(self):
-        # return unicode(self.email)
         return unicode(self.id)
 
     def is_authenticated(self):
         return True
-
 
 class Contract(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -94,6 +90,39 @@ class Contract(db.Model):
         self.spotprice = spotprice 
         self.soliditycodedeliverydate = soliditycodedeliverydate 
         self.soliditycodecancel = soliditycodecancel 
+
+class Calloption(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    blockchainderivativesid = db.Column(db.String(64))
+    buyerethereumaddress = db.Column(db.String(64))
+    sellerethereumaddress = db.Column(db.String(64))
+    expirydate = db.Column(db.Integer)
+    numberofunits = db.Column(db.Integer)
+    assetname = db.Column(db.String(64))
+    strikeprice = db.Column(db.Integer)
+    premium = db.Column(db.Integer)
+    soliditycodeinitial = db.Column(db.String(64))
+    soliditycodeexpirydate = db.Column(db.String(64))
+    soliditycodecancel = db.Column(db.String(64))
+
+    def __repr__(self):
+        return '%s  %s  %s  %s  %s  %s  %s  %s  %s  %s  %s' % (
+        self.blockchainderivativesid, self.buyerethereumaddress, self.sellerethereumaddress, self.expirydate,
+        self.numberofunits, self.assetname, self.strikeprice, self.premium, self.soliditycodeinitial, self.soliditycodeexpirydate, self.soliditycodecancel)
+
+    def __init__(self, blockchainderivativesid, buyerethereumaddress, sellerethereumaddress, expirydate,
+                 numberofunits, assetname, strikeprice, premium, soliditycodeinitial, soliditycodeexpirydate, soliditycodecancel):
+        self.blockchainderivativesid = blockchainderivativesid
+        self.buyerethereumaddress = buyerethereumaddress
+        self.sellerethereumaddress = sellerethereumaddress
+        self.expirydate = expirydate
+        self.numberofunits = numberofunits
+        self.assetname = assetname
+        self.strikeprice = strikeprice
+        self.preumium = premium
+        self.soliditycodeinitial = soliditycodeinitial
+        self.soliditycodeexpirydate = soliditycodeexpirydate
+        self.soliditycodecancel = soliditycodecancel
 
 class Spotprice(db.Model):
     #__tablename__ = 'spotprice'
@@ -137,9 +166,10 @@ db.create_all()
 # allSpotPrices=Spotprice.query.all()
 # print allSpotPrices
 
+# registration and log in process and pages
+
 login_manager = LoginManager()
 login_manager.init_app(app)
-
 SECRET_KEY = 'secretkey'
 USERNAME = 'username'
 PASSWORD = 'password'
@@ -150,12 +180,9 @@ class RegistrationForm(Form):      #possibly not using any more
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    # form = RegistrationForm(request.form)
     if request.method == 'POST':
-        # registered_users = User.query.filter_by(email=form.email.data)
         registered_users = User.query.filter_by(email=request.form['email'])
         if registered_users.count() == 0:
-            # user = User(form.email.data, form.password.data)
             user = User(request.form['email'], request.form['password'])
             db.session.add(user)
             db.session.commit()
@@ -178,14 +205,10 @@ class LoginForm(Form):
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    # form = LoginForm(request.form)
     if request.method == 'POST':
-        # registered_users = User.query.filter_by(
-        #     email=form.email.data)
         registered_users = User.query.filter_by(
             email=request.form['email'])
         if registered_users.count() == 1:
-            # user = User.query.filter_by(email=form.email.data).first()
             user = User.query.filter_by(email=request.form['email']).first()
             user = User.query.get(int(user.id))
             login_user(user)
@@ -205,20 +228,21 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
-@app.route('/mycontracts')
-# @login_required
-def mycontracts():
-    return render_template('mycontracts.html', contracts=Contract.query.all())
-
+# pages of website
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
 @app.route('/futureethereum', methods=["GET", "POST"])
 def main_future():
     error = None
+    spotprice_dictionary = {}
+    for u in Spotprice.query.all():
+        dict = u.__dict__
+        dict_commodity = dict.get('commodity')
+        dict_spotprice = float(dict.get('spotprice'))
+        spotprice_dictionary[dict_commodity] = dict_spotprice
     if request.method == 'POST':
         contract = Contract(str(current_user.id), request.form['buyerethereumaddress'],
                             request.form['sellerethereumaddress'], request.form['deliverydateTimestamp'],
@@ -226,48 +250,76 @@ def main_future():
                             request.form['margin'], request.form['contractfield'], 0, 0, request.form['contractfield2'], request.form['contractfield3'])  #might not need str() #change last one or change deliverydate back 
         db.session.add(contract)
         db.session.commit()
-        return render_template('futureethereum.html', spotprices=Spotprice.query.all())
-    return render_template('futureethereum.html', error=error, spotprices=Spotprice.query.all())
-
+        return render_template('futureethereum.html', spotprices=Spotprice.query.all(), spotpriceslist=json.dumps(spotprice_dictionary))
+    return render_template('futureethereum.html', error=error, spotprices=Spotprice.query.all(), spotpriceslist=json.dumps(spotprice_dictionary))
 
 @app.route('/calloptionethereum', methods=["GET", "POST"])
 def main_call_option():
     error = None
-    #print Spotprice.query.all()
-    # list_of_dictionaries = []
     spotprice_dictionary = {}
     for u in Spotprice.query.all():
-        # little_list = []
         dict = u.__dict__
         dict_commodity = dict.get('commodity')
-        # little_list.append(dict_commodity)
         dict_spotprice = float(dict.get('spotprice'))
         spotprice_dictionary[dict_commodity] = dict_spotprice
-        # little_list.append(dict_spotprice)
-        # list_of_dictionaries.append(little_list)
-        #list_of_dictionaries(u.__dict__)
-    # print list_of_dictionaries
-    # print spotprice_dictionary
     if request.method == 'POST':
-        # print request.form['soliditycodeinitial']
+        contract = Calloption(str(current_user.id), request.form['buyerethereumaddress'],
+                            request.form['sellerethereumaddress'], request.form['expirydateTimestamp'],
+                            request.form['numberofunits'], request.form['assetname'], request.form['strikeprice'],
+                            request.form['premium'], request.form['soliditycodeinitial'], request.form['contractfield2'], request.form['contractfield3'])  #might not need str() #change last one or change deliverydate back
+        db.session.add(contract)
+        db.session.commit()
+        return render_template('calloptionethereum.html', spotprices=Spotprice.query.all(), spotpriceslist=json.dumps(spotprice_dictionary))
+    return render_template('calloptionethereum.html', error=error, spotprices=Spotprice.query.all(), spotpriceslist=json.dumps(spotprice_dictionary)) #spotpricesjson=json.dumps(Spotprice.query.all()))
+
+@app.route('/putoptionethereum', methods=["GET", "POST"])
+def main_put_option():
+    error = None
+    spotprice_dictionary = {}
+    for u in Spotprice.query.all():
+        dict = u.__dict__
+        dict_commodity = dict.get('commodity')
+        dict_spotprice = float(dict.get('spotprice'))
+        spotprice_dictionary[dict_commodity] = dict_spotprice
+    if request.method == 'POST':
         contract = Contract(str(current_user.id), request.form['buyerethereumaddress'],
                             request.form['sellerethereumaddress'], request.form['deliverydateTimestamp'],
                             request.form['numberofunits'], request.form['commodityname'], request.form['price'],
                             request.form['margin'], request.form['soliditycodeinitial'], 0, 0, request.form['contractfield2'], request.form['contractfield3'])  #might not need str() #change last one or change deliverydate back
         db.session.add(contract)
         db.session.commit()
-        return render_template('calloptionethereum.html', spotprices=Spotprice.query.all(), spotpriceslist=json.dumps(spotprice_dictionary))
-    return render_template('calloptionethereum.html', error=error, spotprices=Spotprice.query.all(), spotpriceslist=json.dumps(spotprice_dictionary)) #spotpricesjson=json.dumps(Spotprice.query.all()))
+        return render_template('putoptionethereum.html', spotprices=Spotprice.query.all(), spotpriceslist=json.dumps(spotprice_dictionary))
+    return render_template('putoptionethereum.html', error=error, spotprices=Spotprice.query.all(), spotpriceslist=json.dumps(spotprice_dictionary)) #spotpricesjson=json.dumps(Spotprice.query.all()))
 
 
-@app.route('/putoptionethereum')
-def main_put_option():
-    return render_template('putoptionethereum.html')
-
-
-@app.route('/swapethereum')
+@app.route('/swapethereum', methods=["GET", "POST"])
 def main_swap():
-    return render_template('swapethereum.html')
+    error = None
+    spotprice_dictionary = {}
+    for u in Spotprice.query.all():
+        dict = u.__dict__
+        dict_commodity = dict.get('commodity')
+        dict_spotprice = float(dict.get('spotprice'))
+        spotprice_dictionary[dict_commodity] = dict_spotprice
+    if request.method == 'POST':
+        contract = Contract(str(current_user.id), request.form['buyerethereumaddress'],
+                            request.form['sellerethereumaddress'], request.form['deliverydateTimestamp'],
+                            request.form['numberofunits'], request.form['commodityname'], request.form['price'],
+                            request.form['margin'], request.form['soliditycodeinitial'], 0, 0, request.form['contractfield2'], request.form['contractfield3'])  #might not need str() #change last one or change deliverydate back
+        db.session.add(contract)
+        db.session.commit()
+        return render_template('swapethereum.html', spotprices=Spotprice.query.all(), spotpriceslist=json.dumps(spotprice_dictionary))
+    return render_template('swapethereum.html', error=error, spotprices=Spotprice.query.all(), spotpriceslist=json.dumps(spotprice_dictionary)) #spotpricesjson=json.dumps(Spotprice.query.all()))
+
+
+@app.route('/mycontracts')
+# @login_required
+def mycontracts():
+    return render_template('mycontracts.html', contracts=Contract.query.all(), calloptions=Calloption.query.all())
+
+@app.route('/tutorial')
+def tutorial():
+    return render_template('tutorial.html')
 
 @app.route("/spotprices", methods=["GET", "POST"])   #changed the href here
 def spotprices():
@@ -284,19 +336,21 @@ def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static', 'images'),
                                'favicon.ico', mimetype='image/png')
 
+# RESTful API
+
 class SpotpriceApi(Resource):
 
-    def put(self, spotpriceapi):  #chanted from put
-        # newprice = request.form['spotprice']
-        # spotpricetoupdate = Spotprice.query.filter_by(commodity=request.form['commodity']).first()
-        # spotpricetoupdate.spotprice = newprice
-        # db.session.commit()
+    def put(self, spotpriceapi):
         commodity_response = request.form['commodity']
         if len(commodity_response) == 0:
             abort(404)
         spotprice_response = request.form['spotprice']
         if len(spotprice_response) == 0:
             abort(404)
+        if type(request.form['commodity']) != unicode:
+            abort(400)
+        if type(request.form['spotprice']) != unicode:
+            abort(400)
         try :
             spotpricetoupdate = Spotprice.query.filter_by(commodity=request.form['commodity']).first()
             newprice = request.form['spotprice']
@@ -309,12 +363,6 @@ class SpotpriceApi(Resource):
         return {}
 
 api.add_resource(SpotpriceApi, '/<string:spotpriceapi>')
-
-#so I think the way to trigger this is to send: curl http://localhost:5000/todo1 -d "data=Remember the milk" -X PUT
-#or curl -H 'Content-Type: application/json' -X PUT -d '{"commodity": "naturalgas", "spotprice":1.22}' http://0.0.0.0:8080/spotpriceapi
-
-
-
 
 if __name__ == '__main__':
     # port = int(os.environ.get("PORT", 33507))
