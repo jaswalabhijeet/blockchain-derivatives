332b82aab0b48c47c3514fa0f97745fc8ad20907
Bill Marino <billmarino@gmail.com>
2015-12-09 11:00:23 -0500

    Switch Method of Testing Redirects

--- pythonunittest.py
+++ pythonunittest.py
@@ -155,8 +155,8 @@ class AppTestCase(unittest.TestCase):
     def test_register_then_login_redirect(self):
         self.app.post('/register', data=dict(email='username@email.com', password='password'), follow_redirects=True)
         response = self.app.post('/login', data=dict(email='username@email.com', password='password'), follow_redirects=True)
-        self.assertEqual(response.request.path, url_for('index'))
-        # self.assertEqual(response.location, url_for('index', _external=True))
+        #self.assertEqual(response.request.path, url_for('index'))
+        self.assertEqual(response.location, url_for('index', _external=True))
 
     def test_register_then_login_then_logout(self):
         self.app.post('/register', data=dict(email='username@email.com', password='password'), follow_redirects=True)
